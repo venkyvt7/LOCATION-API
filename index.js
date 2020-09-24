cf7b8bb87d5d3d0075459d6e5b2bca806cf4af63
venkyvt7 <venkateshp.tripathi@gmail.com>
2020-09-24 12:23:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const app=express();
 
 const bodyparser=require('body-parser');
 
-const PORT = process.env.PORT || 5001;
+
 app.use(bodyparser.json());
 
 
 const router=require('./routes/api');
 app.use(router);
-
-app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
+app.listen(process.env.PORT || 3000);
